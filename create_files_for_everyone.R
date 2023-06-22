6d76f44765aafff381640b2dd76b69b53dda9eac
Theile <theile@demogr.mpg.de>
2023-06-22 19:34:50 +0200

    added another loop to create data it works! gitignore ignores csv now

--- create_files_for_everyone.R
+++ create_files_for_everyone.R
@@ -4,13 +4,25 @@ if (!dir.exists(dir)) {
   dir.create(dir)
 }
 
-# Loop to create the files
+# Loop to create the files for everyone
 for (i in 1:30) {
   # Generate the file name with the number
   file_name <- paste0(dir, "/file_", i, ".txt")
   
   # Create and write content to the file
   writeLines("This is some content.", file_name)
+  
+  # Print a message indicating the file creation
+  cat("Created file:", file_name, "\n")
+}
+
+# Loop to create some useless files
+for (i in 1:30) {
+  # Generate the file name with the number
+  file_name <- paste0(dir, "/file_", i, ".csv")
+  
+  # Create and write content to the file
+  writeLines("This, is, some, data!\n1,2,3,4", file_name)
   
   # Print a message indicating the file creation
   cat("Created file:", file_name, "\n")
